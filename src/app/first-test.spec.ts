aa1db1d70cfbce785da0290d61b0000771010597
clover <clover.hsc@gmail.com>
2023-02-12 15:26:48 +0800

    Chapter 1. First Test

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,18 @@
+describe('First Test', () => {
+  let testVariable: any;
+
+  beforeEach(() => {
+    testVariable = {};
+  });
+
+  it('should be return true', () => {
+    // Arrange
+    testVariable.a = false;
+
+    // Act
+    testVariable.a = true;
+
+    // Assert
+    expect(testVariable.a).toBe(true);
+  })
+})
